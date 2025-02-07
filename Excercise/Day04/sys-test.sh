e5fa709177b40226ecf08e7a78d90f8c06c08acd
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-02-07 23:34:30 +0700

    try

--- Excercise/Day04/sys-test.sh
+++ Excercise/Day04/sys-test.sh
@@ -1,2 +1,13 @@
-docker-compose up redis nginx kafka cassandra scala-backend processing-backend scala-backend2 test
-#docker-compose up nginx kafka cassandra scala-backend
+#!/bin/bash
+
+docker-compose up redis nginx kafka cassandra scala-backend processing-backend scala-backend2 test --abort-on-container-exit --exit-code-from test
+# Capture the exit code of the test container
+EXIT_CODE=$?
+
+# Check if the exit code is non-zero
+if [ $EXIT_CODE -ne 0 ]; then
+  docker-compose down
+  echo "Error: Test container exited with code $EXIT_CODE"
+  # Optionally, you can exit the script immediately to prevent further actions
+  exit $EXIT_CODE
+fi
--- Excercise/Day04/test/test.sh
+++ Excercise/Day04/test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -f http://nginx || exit 1
-# curl -f http://scala-backend2:8081/heatmap || exit 1
+curl -f http://scala-backend2:8081/heatmap || exit 1
 curl -f http://scala-backend:8080/healthcheck || exit 1
